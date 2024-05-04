2b4380086e3947d5cdf025a1589b3e382ea7449e
ZhurbaCreations <104913708+ZhurbaCreations@users.noreply.github.com>
2024-05-04 22:51:10 +0300

    Update initFirebase.ts

--- lib/initFirebase.ts
+++ lib/initFirebase.ts
@@ -9,11 +9,11 @@ export default function initializeFirebaseClient(): {
 } {
   const firebaseConfig = initializeApp({
     apiKey: process.env.NEXT_PUBLIC_API_KEY || "AIzaSyCv_MKYdPt7xV4rt4w8Ob30mNG4g2S0Lus",
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN || "yoola-auth-api.firebaseapp.com",
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID || "yoola-auth-api",
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET || "yoola-auth-api.appspot.com",
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID || "833741109645",
+    appId: process.env.NEXT_PUBLIC_APP_ID || "833741109645:web:f27bea3694e88d019ae5c4",
   });
 
   const db = getFirestore(firebaseConfig);
